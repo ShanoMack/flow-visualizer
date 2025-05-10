d218f71c5565bbde184bcef1f585e52dbde590e9
ImShaneTurner <shaneturner@live.co.za>
2025-05-10 21:25:03 +0200

    more folder cleanup

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => 
+{
+  console.log("Shane's app is working");
   const button = document.getElementById('open-sidebar');
-  if (button) {
+  console.log(button);
+  if (button) 
+  {
     button.addEventListener('click', () => {
       miro.board.ui.openPanel({ url: 'sidebar.html' });
     });
