1875535132928dfce282bf511830bd05e3762900
ImShaneTurner <shaneturner@live.co.za>
2025-05-10 20:56:08 +0200

    testing

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 async function addSticky() {
     const stickyNote = await miro.board.createStickyNote({
-        content: 'Hello, World!', 
+        content: 'Hello, from my app', 
     }); 
     
     await miro.board.viewport.zoomTo(stickyNote); 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export async function init() {
+    console.log("Working app");
     miro.board.ui.on('icon:click', async () => {
         await miro.board.ui.openPanel({ url: 'app.html' }); 
     }); 
